eb81072f9435c7ccef5c82e0cb8e0a413d59bcf0
kirkiboi <168092735+kirkiboi@users.noreply.github.com>
2025-03-06 14:37:10 +0000

    everything updated

--- functions.js
+++ functions.js
@@ -1,17 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const div1 = document.getElementById("div1");
+    const div2 = document.getElementById("div2");
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const audio = document.getElementById("audio");
 
-const div1 = document.getElementById("div1");
-const div2 = document.getElementById("div2");
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
+    button1.addEventListener('click', function () {
+        div1.style.display = 'none';
+        div2.style.display = 'block';
+        document.body.style.backgroundImage = 'url(cottonwithoutemi.jpg)';
 
-button1.addEventListener('click', function(){
-    div1.style.display = 'none';
-    div2.style.display = 'block';
-    document.body.style.backgroundImage = 'url(cottonwithoutemi.jpg)';
-})
-button2.addEventListener('click', function(){
-    button2.innerHTML = "gahi jud kag ulo. anyway, imy yot and sorry sad :<<"
-})
+        const button3 = document.getElementById("music-button");
+        button3.addEventListener('click', function () {
+            audio.play();
+        });
+    });
 
+    button2.addEventListener('click', function () {
+        button2.innerHTML = "gahi jud kag ulo. anyway, imy yot and sorry sad :<<";
+    });
 });
